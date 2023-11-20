58b795daf2768b1f82dd60eb4361204604b4f9df
Demetrius Vissarion <demetrius.vissarion@gmail.com>
2023-11-20 13:46:59 +0000

    completed exercise

--- lib/time_error.py
+++ lib/time_error.py
@@ -1,13 +1,14 @@
 import time
 
 class TimeError:
-    def __init__(self, requester) -> None:
+    def __init__(self, requester, time) -> None:
         self.requester = requester
+        self.time = time
 
     # Returns difference in seconds between the time on an external server
     # and the time on this computer
     def error(self):
-        return self._get_server_time() - time.time()
+        return self._get_server_time() - self.time
 
     # The underscore denotes this is a private method not to be called from the
     # outside. You also shouldn't stub it in your tests. So if your tests contain
@@ -15,12 +16,14 @@ class TimeError:
     def _get_server_time(self):
         response = self.requester.get("https://worldtimeapi.org/api/ip")
         json = response.json()
+        print(f'my_time: {self.time}')
+        print(f'JSON Response: {json}')
         return json["unixtime"]
 
 
 # Usage
 # =====
-import requests
-time_error = TimeError(requests)
-time_error.error() # => "Why not: Learn how to use a french press"
-# time_error.error() # => "Why not: Hold a video game tournament with some friends"
+# import requests
+# my_time = time.time()
+# time_error = TimeError(requests, my_time)
+# print(time_error.error())
--- tests/test_time_error.py
+++ tests/test_time_error.py
@@ -6,18 +6,15 @@ from unittest.mock import Mock
 def test_calls_api_to_provide_time():
     requests_mock = Mock(name="requests") # This name is just for readability
     response_mock = Mock(name="response")
+    time_mock = 1700485279.628256
 
-    # We tell `requester_mock` to return `response_mock` 
-    # when we call `get()` on it.
     requests_mock.get.return_value = response_mock
 
     # We tell `response_mock` to return a sample output of the API when
     # we call `json()` on it.
     # I got this sample using `curl "https://www.boredapi.com/api/activity"`.
-    response_mock.json.return_value = {
-        
-    }
+    response_mock.json.return_value = {'abbreviation': 'GMT', 'client_ip': '82.163.117.26', 'datetime': '2023-11-20T13:01:19.684576+00:00', 'day_of_week': 1, 'day_of_year': 324, 'dst': False, 'dst_from': None, 'dst_offset': 0, 'dst_until': None, 'raw_offset': 0, 'timezone': 'Europe/London', 'unixtime': 1700485279, 'utc_datetime': '2023-11-20T13:01:19.684576+00:00', 'utc_offset': '+00:00', 'week_number': 47}
 
-    time_error = TimeError(requests_mock)
+    time_error = TimeError(requests_mock, time_mock)
     result = time_error.error()
-    assert result == ""
+    assert result == -0.62825608253479
